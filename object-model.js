5b99ea776da2e004dd487f3108e023141df6346b
Fernando Reis Guimarães <fernandobhz@gmail.com>
2021-08-09 19:55:38 -0300

    initial commit

---
+++ object-model.js
@@ -0,0 +1,12 @@
+{
+  decryptCode: `
+    const decrypt = (encrypted, password) {
+      .....
+    }
+  `,
+  encryptCode: {
+    const encrypt = (plaintext, password)
+  },
+  salt: 'dfgqwyurh342957810jhunkljn897osdaf90',
+  encryptedData: 'f2ouiybh123458907yxza\lkjbq3498123iugbdadf871234123=='
+}
