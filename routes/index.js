04b040a27bb1a9d4625b3d9c1696ddf45ee21c3a
Ian <lsouza.ian@gmail.com>
2021-09-10 11:04:25 -0300

    chore: create html pages

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'SecNote' });
 });
+
+router.get('/encrypt-data', (req, res) => res.render('encrypt-data-get'));
+
+router.post('/encrypt-data', (req, res) => {
+  const { text, password } = req.body;
+
+  res.render('encrypt-data-post', { text, password });
+})
 
 module.exports = router;
