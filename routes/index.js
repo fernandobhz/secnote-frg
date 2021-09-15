028847302f343fc518f137342fa61dd71a977615
Fernando Reis Guimarães <fernandobhz@gmail.com>
2021-09-15 15:54:21 -0300

    chore: api routes

--- routes/index.js
+++ routes/index.js
@@ -10,29 +10,28 @@ router.get("/encrypt-data", (req, res) => res.render("encrypt-data-get"));
 
 router.post("/encrypt-data", (req, res) => {
   const { text, password } = req.body;
-
-  // todo: transform this function into an async one
   const encrypted = encrypt(text, password);
+  res.render("encrypt-data-post", { text, password, encrypted });
+});
 
-  console.log(`encrypting`, { encrypted });
-
-  res.render("encrypt-data-post", {
-    text,
-    password,
-    encrypted,
-  });
+router.post(`/api/encrypt`, (req, res) => {
+  const { decrypted, password } = req.body;
+  const encrypted = encrypt(decrypted, password);
+  res.json(encrypted);
 });
 
 router.get("/decrypt-data", (req, res) => res.render("decrypt-data-get"));
 
 router.post("/decrypt-data", (req, res) => {
   const { encrypted, password } = req.body;
-
-  console.log(`decrypting`, { encrypted });
-
   const text = decrypt(encrypted, password);
-
   res.render("decrypt-data-post", { encrypted, password, text });
 });
+
+router.post(`/api/decrypt`, (req, res) => {
+  const { encrypted, password } = req.body;
+  const decrypted = decrypt(encrypted, password);
+  res.json(decrypted);
+})
 
 module.exports = router;
