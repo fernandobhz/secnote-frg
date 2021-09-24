041c80487277a141afa9c377615d658172e203fd
Fernando Reis Guimarães <fernandobhz@gmail.com>
2021-09-24 16:36:47 -0300

    feat: small app that decrypt

---
+++ decrypter/index.js
@@ -0,0 +1,14 @@
+const { scryptSync, createDecipheriv } = require("crypto");
+const { Buffer } = require("buffer");
+
+decrypt = (encryptedIv, password) => {
+  const { encrypted, iv } = JSON.parse(Buffer.from(encryptedIv, `base64`).toString(`utf8`));
+  const algorithm = "aes-256-cbc";
+  const key = scryptSync(password, `salt`, 32);
+  const ivBuff = Buffer.from(iv, `hex`);
+  const decipher = createDecipheriv(algorithm, key, ivBuff);
+  const decrypted = decipher.update(encrypted, `hex`, `utf8`) + decipher.final(`utf8`);
+  return decrypted;
+};
+const [nodePath, scriptPath, password, encryptedIv] = process.argv;
+console.log(decrypt(encryptedIv, password));
