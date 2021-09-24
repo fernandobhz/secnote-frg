0c4dc96564c91554590d08a9774b541ee83624ae
Fernando Reis Guimarães <fernandobhz@gmail.com>
2021-09-24 14:35:06 -0300

    chore: prettier

--- services/data-encryption.js
+++ services/data-encryption.js
@@ -1,20 +1,15 @@
-const {
-  scryptSync,
-  randomBytes,
-  createCipheriv,
-  createDecipheriv,
-} = require('crypto');
-const { Buffer } = require('buffer');
+const { scryptSync, randomBytes, createCipheriv, createDecipheriv } = require("crypto");
+const { Buffer } = require("buffer");
 
 exports.encrypt = (text, password) => {
-  const algorithm = 'aes-256-cbc';
+  const algorithm = "aes-256-cbc";
   const key = scryptSync(password, `salt`, 32);
   const ivBuff = randomBytes(16);
   const cipher = createCipheriv(algorithm, key, ivBuff);
   const encrypted = cipher.update(text, `utf8`, `hex`) + cipher.final(`hex`);
   const iv = ivBuff.toString(`hex`);
-  
-  const encryptedIv = Buffer.from(JSON.stringify({ encrypted, iv}), `utf8`).toString(`base64`);
+
+  const encryptedIv = Buffer.from(JSON.stringify({ encrypted, iv }), `utf8`).toString(`base64`);
 
   console.log(`Encrypting... Encrypted, iv: `, encrypted, iv);
   return encryptedIv;
@@ -23,10 +18,10 @@ exports.encrypt = (text, password) => {
 exports.decrypt = (encryptedIv, password) => {
   const { encrypted, iv } = JSON.parse(Buffer.from(encryptedIv, `base64`).toString(`utf8`));
   console.log(`Decrypting... Encrypted, iv: `, encrypted, iv);
-  
-  const algorithm = 'aes-256-cbc';
+
+  const algorithm = "aes-256-cbc";
   const key = scryptSync(password, `salt`, 32);
-  const ivBuff = Buffer.from(iv, `hex`); 
+  const ivBuff = Buffer.from(iv, `hex`);
   const decipher = createDecipheriv(algorithm, key, ivBuff);
   const decrypted = decipher.update(encrypted, `hex`, `utf8`) + decipher.final(`utf8`);
 
