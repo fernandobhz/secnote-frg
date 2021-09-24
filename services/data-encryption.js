41bc193ad5d49491b78378e4aa1d16be08908a2b
Fernando Reis Guimarães <fernandobhz@gmail.com>
2021-09-24 14:36:24 -0300

    chore: cleanup

--- services/data-encryption.js
+++ services/data-encryption.js
@@ -8,22 +8,16 @@ exports.encrypt = (text, password) => {
   const cipher = createCipheriv(algorithm, key, ivBuff);
   const encrypted = cipher.update(text, `utf8`, `hex`) + cipher.final(`hex`);
   const iv = ivBuff.toString(`hex`);
-
   const encryptedIv = Buffer.from(JSON.stringify({ encrypted, iv }), `utf8`).toString(`base64`);
-
-  console.log(`Encrypting... Encrypted, iv: `, encrypted, iv);
   return encryptedIv;
 };
 
 exports.decrypt = (encryptedIv, password) => {
   const { encrypted, iv } = JSON.parse(Buffer.from(encryptedIv, `base64`).toString(`utf8`));
-  console.log(`Decrypting... Encrypted, iv: `, encrypted, iv);
-
   const algorithm = "aes-256-cbc";
   const key = scryptSync(password, `salt`, 32);
   const ivBuff = Buffer.from(iv, `hex`);
   const decipher = createDecipheriv(algorithm, key, ivBuff);
   const decrypted = decipher.update(encrypted, `hex`, `utf8`) + decipher.final(`utf8`);
-
   return decrypted;
 };
